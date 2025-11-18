37be22167633ba129f61e942e35659984254492a
Tatsuya Sakunaga <sakunaga@kract.jp>
2025-11-18 12:19:00 +0900

    Add addon preferences for GZR Keymap, including a tab for keymap management and UI enhancements for better user interaction with keymap settings.

--- gzr-keymap/__init__.py
+++ gzr-keymap/__init__.py
@@ -8,12 +8,73 @@ bl_info = {
 }
 
 import bpy
+from bpy.props import EnumProperty
+from bpy.types import AddonPreferences
+import rna_keymap_ui
 
 addon_keymaps = []
 
 
+class GZR_KEYMAP_AddonPreferences(AddonPreferences):
+    bl_idname = __name__
+
+    tab_addon_menu: EnumProperty(
+        name="tab",
+        description="",
+        items=[
+            ('KEYMAP', "Keymap", "", "EVENT_A", 0),
+        ],
+        default='KEYMAP'
+    )
+
+    def draw(self, context):
+        layout = self.layout
+
+        row = layout.row(align=True)
+        row.prop(self, "tab_addon_menu", expand=True)
+
+        if self.tab_addon_menu == "KEYMAP":
+            box = layout.box()
+            col = box.column()
+            col.label(text="Keymap List:")
+
+            wm = bpy.context.window_manager
+            kc = wm.keyconfigs.user
+            old_km_name = ""
+            old_id_l = []
+
+            for km_add, kmi_add in addon_keymaps:
+                for km_con in kc.keymaps:
+                    if km_add.name == km_con.name:
+                        km = km_con
+                        break
+
+                for kmi_con in km.keymap_items:
+                    if kmi_add.idname == kmi_con.idname:
+                        if not kmi_con.id in old_id_l:
+                            kmi = kmi_con
+                            old_id_l.append(kmi_con.id)
+                            break
+
+                try:
+                    if not km.name == old_km_name:
+                        col.label(text=str(km.name), icon="DOT")
+                    col.context_pointer_set("keymap", km)
+                    rna_keymap_ui.draw_kmi([], kc, km, kmi, col, 0)
+                    col.separator()
+                    old_km_name = km.name
+                except:
+                    pass
+
+
+classes = (GZR_KEYMAP_AddonPreferences,)
+
+
 def register():
     """Register addon"""
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
 
@@ -68,6 +129,9 @@ def register():
 
 def unregister():
     """Unregister addon"""
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+
     for (km, kmi) in addon_keymaps:
         try:
             km.keymap_items.remove(kmi)
